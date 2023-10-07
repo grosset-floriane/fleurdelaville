dc0bc780e6028ec0d7030b03e69b22a8248ba0a4
Floriane Grosset <fl.grosset@gmail.com>
2023-10-07 21:42:20 +0200

    💄 style header mobile

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,12 +1,28 @@
 import React from 'react'
 import MainNav from '../MainNav/MainNav'
+import styled from '@emotion/styled'
+import { SPACING, TITLE_FONT } from 'context/stylesConstants'
 
 const Header = () => {
+  const StyledHeader = styled.header`
+    position: fixed;
+    top: 0;
+    left: 0;
+    padding: 0 ${SPACING * 2}rem;
+    z-index: 999;
+  `
+
+  const SiteTitle = styled.a`
+    font-family: ${TITLE_FONT};
+
+    font-size: 4.5rem;
+  `
+
   return (
-    <header>
-      <a href="/">Fleur Delaville</a>
+    <StyledHeader>
+      <SiteTitle href="/">Fleur Delaville</SiteTitle>
       <MainNav />
-    </header>
+    </StyledHeader>
   )
 }
 
