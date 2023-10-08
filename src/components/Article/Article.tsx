079ef67640c90ee01b3ea98d333fd930b70c8efc
Floriane Grosset <fl.grosset@gmail.com>
2023-10-08 21:04:08 +0200

    💄 add mobile style articles

--- src/components/Article/Article.tsx
+++ src/components/Article/Article.tsx
@@ -1,16 +1,54 @@
 import React from 'react'
 import { useContext } from 'react'
 import { storeContext } from '../../context/storeContext'
+import FeaturedImage from 'components/ArchiveContent/FeaturedImage'
+import styled from '@emotion/styled'
+import { SPACING } from 'context/stylesConstants'
 
 const Article = () => {
   const posts = useContext(storeContext)
 
   if (!posts) return
+  const Figure = styled.figure`
+    min-height: 75vh;
 
-  return posts.map(({ title, content, slug }) => (
+    img {
+      position: absolute;
+      top: 0;
+      left: 0;
+    }
+  `
+
+  const Section = styled.section`
+    padding: ${SPACING * 2}rem;
+    p {
+      line-height: 1.5;
+    }
+  `
+  const StickyTitle = styled.h1`
+    position: sticky;
+    top: 9rem;
+    right: 0;
+    text-align: center;
+    padding: 0 ${SPACING * 2}rem;
+    font-size: 6rem;
+    font-weight: normal;
+    color: white;
+    z-index: 9999;
+  `
+
+  return posts.map(({ title, content, slug, _links }) => (
     <article key={slug}>
-      <h1>{title.rendered}</h1>
-      <div
+      <Figure>
+        <FeaturedImage
+          url={_links['wp:featuredmedia'][0].href}
+          alt={title.rendered}
+          isSingle
+        />
+        <StickyTitle>{title.rendered}</StickyTitle>
+      </Figure>
+
+      <Section
         className="post-content"
         dangerouslySetInnerHTML={{ __html: content.rendered }}
       />
