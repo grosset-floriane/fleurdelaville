27390fe0c4b566077d5842c52570ec3fd3816816
Floriane Grosset <fl.grosset@gmail.com>
2023-10-21 00:34:23 +0200

    🔍 add basic SEO tags

--- public/index.php
+++ public/index.php
@@ -3,8 +3,8 @@
 ?>
 <!DOCTYPE html>
 <html lang="en">
-<head>
-<meta charset="utf-8" />
+  <head>
+    <meta charset="utf-8" />
     <link rel="shortcut icon" href="%PUBLIC_URL%/logo.png" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <meta name="theme-color" content="#000000" />
@@ -19,11 +19,11 @@
     -->
     <link rel="manifest" href="<?php echo $TEMPLATE_PATH; ?>/manifest.json" />
 
-      <!-- Fonts -->
-  <link rel="preconnect" href="https://fonts.googleapis.com">
-  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-  <link href="https://fonts.googleapis.com/css2?family=Lato:wght@400;700&family=Reenie+Beanie&display=swap"
-    rel="stylesheet">
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Lato:wght@400;700&family=Reenie+Beanie&display=swap"
+      rel="stylesheet">
     <!--
         If you're reading this from "view source" in your browser, it might not make sense as
         these tokens have already been evaluated and replaced, even in this remark blurb.
@@ -39,8 +39,61 @@
     -->
     <script src="%PUBLIC_URL%/react-src/src/assets/scripts/background-check.min.js"></script>
     <script src="%PUBLIC_URL%/react-src/src/assets/scripts/main.js"></script>
-    <title>React WordPress Theme</title>
-</head>
+    <title>Fleur Delaville, artist & box maker</title>
+
+    <meta property="og:site_name" content="Fleur Delaville" />
+    <meta property="og:locale" content="en_UK" />
+    <meta property="og:type" content="website" />
+    <meta
+      property="og:image"
+      content="%PUBLIC_URL%/react-src/src/assets/images/banner.jpg"
+    />
+    <meta
+      property="og:image:width"
+      content="1200"
+    />
+    <meta
+      property="og:image:height"
+      content="630"
+    />
+    <meta
+      property="og:image:alt"
+      content="Written Fleur Delaville, and a cropped view of a building top on the blue sky"
+    />
+
+          
+    <meta name="twitter:card" content="summary_large_image" />
+    <meta property="twitter:url" content="https://fleurdelaville.net/"/>
+    <meta name="twitter:site" content="@FlosWebdesign" />
+    <meta
+      name="twitter:image"
+      content="https://fleurdelaville.net/assets/images/banner.jpg"
+    />
+            <meta
+      property="twitter:image:alt"
+      content="Written Fleur Delaville, and a cropped view of a building top on the blue sky"
+    />
+    <meta name="twitter:creator" content="@FlosWebdesign" />
+
+    <meta name="title" content="Fleur Delaville, artist & box maker" data-react-helmet="true"/>
+    <meta name="description" content="Fleur Delaville is a French artist, based in Hamburg, Germany. She is working with analogue and digital photography, and design machines inspired by pre-cinematic toys." data-react-helmet="true"/>
+
+    <meta property="og:title" content="Fleur Delaville, artist & box maker" data-react-helmet="true" />
+    <meta property="og:description" content="Fleur Delaville is a French artist, based in Hamburg, Germany. She is working with analogue and digital photography, and design machines inspired by pre-cinematic toys." data-react-helmet="true" />
+    <meta
+      property="og:url"
+      content="https://fleurdelaville.net/"
+      data-react-helmet="true"
+    />
+            
+
+    <meta name="twitter:title" content="Fleur Delaville, artist & box maker" data-react-helmet="true" />
+    <meta
+      name="twitter:description"
+      content="Fleur Delaville is a French artist, based in Hamburg, Germany. She is working with analogue and digital photography, and design machines inspired by pre-cinematic toys."
+      data-react-helmet="true"
+    />
+  </head>
     <body>
     <noscript>
         You need to enable JavaScript to run this app.
